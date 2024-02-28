c13ccb23cf5b99c6c5c8c87dba2eb13f74777cfa
huy.start17 <huyhuy17@gmail.com>
2024-02-28 23:06:04 +0700

    aa

---
+++ src/assets.js
@@ -0,0 +1,34 @@
+const {Sprite, Tile} = require("./classes");
+const TileAsset = require("./assets/sheet.json");
+const Misc = require("./assets/stuff.json");
+
+const getTileFromAsset = (asset, id) => asset[id];
+
+const Assets = {
+    tiles: {},
+    imgs: {},
+    getTile: (id) => this.tiles[id],
+    importTile: (asset, image, assetID, id = assetID) => {
+        if (asset[assetID] = undefined) {
+            console.error(`item not found"`);
+            return 0;
+        };
+
+        const tile = new Tile(id);
+        if(asset[assetID].name) {tile.localName = asset[assetID].name} else {tile.localName = assetID};
+        if(asset[assetID].w) {tile.width = asset[assetID].w} else {tile.width = 1};
+        if(asset[assetID].h) {tile.height = asset[assetID].h} else {tile.height = 1};
+        if(asset[assetID].ground) {tile.isGround = !0} else {tile.isGround = !1};
+        tile.sprite = new Sprite(image, asset[assetID].img.x, asset[assetID].img.y, asset[assetID].img.w, asset[assetID].img.h);
+    }
+}
+
+console.log(TileAsset)
+
+
+
+const tileSheet = new Image(1500, 600);
+tileSheet.src = "./assets/sheet.png";
+const miscSheet = new Image(700, 1600);
+miscSheet.src = "./assets/stuff.png";
+
---
+++ src/classes.js
@@ -0,0 +1,117 @@
+class Sprite {
+    constructor(
+        src = new Image(), //source image
+        x = 0, y = 0, width = 16, height = 16, //region of the image that the sprite is in
+    ) {
+        this.src = src;
+        this.x = x; this.y = y; this.width = width; this.height = height;
+    }
+}
+
+class Tile {
+    static categories = ["tiles", "groundTiles", "waypoints", "spawnPoints", "ammo", "spawningPointsRed", "spawningPointsBlue", "redFlags", "blueFlags"]
+    
+    constructor(
+        id = 0, //numeric ID
+        localName = "unknown", //tile's local display name in UI
+        category = "tiles", //tile category
+        width = 1, height = 1, //tile size
+        isGround = false,
+        sprite = null,
+    ) {
+        this.id = id;
+        this.localName = localName;
+        this.category = category;
+        this.width = width; this.height = height;
+        this.pathing = pathing; 
+        this.isGround = isGround;
+        this.sprite = sprite;
+        this.params = {};
+    }
+}
+class Teleporter extends Tile {
+    constructor(
+        id = 0, //numeric ID
+        localName = "unknown", 
+        width = 1, height = 1, 
+        sprite = null,
+        channel = 1,
+    ) {
+        super(id, localName, "tiles", width, height, false, sprite);
+        this.params.channel = channel;
+    }
+}
+
+class Conveyor extends Tile {
+    constructor(
+        id = 0, //numeric ID
+        localName = "unknown", 
+        width = 1, height = 1, 
+        sprite = null,
+        shiftX = 1,
+        shiftY = 0,
+    ) {
+        super(id, localName, "tiles", width, height, false, sprite);
+        this.params.shiftX = shiftX;
+        this.params.shiftY = shiftY;
+    }
+}
+
+class Map {
+    constructor(
+        name = "Untitled",
+        description = "",
+        maxPlayers = 16,
+        hidden = false, //whether this map is not shown up in the in-game map list
+        closed = false, //whether this map is not in in-game map rotation
+        type = 0, //default gamemode of this map
+        width = 32, height = 32, //map size
+        tiles = {},
+        groundTiles = {},
+        waypoints = {},
+        spawnPoints = {},
+        spawnRed = {},
+        spawnBlue = {},
+        redFlags = {},
+        blueFlags = {},
+        pickups = {},
+
+    ) {
+        this.name = name;
+        this.description = description;
+        this.maxPlayers = maxPlayers;
+        this.invisible = hidden;
+        this.closed = closed;
+        this.type = type;
+        this.width = width; this.height = height;
+        this.tiles = tiles;
+        this.groundTiles = groundTiles;
+        this.waypoints = waypoints;
+        this.spawnPoints = spawnPoints;
+        this.spawningPointsBlue = spawnBlue;
+        this.spawningPointsRed = spawnRed;
+        this.redFlags = redFlags;
+        this.blueFlags = blueFlags;
+        this.ammo = pickups;
+    }
+
+    addTile(tile, x, y) {
+        const posKey = x + "&" + y;
+        if (!this[tile.category][posKey]) {
+            this[tile.category][posKey] = []
+        }
+        const tileObject = {};
+        tileObject.id = tile.id;
+        tileObject.x = tile.x;
+        tileObject.y = tile.y;
+
+        for (let param in tile.params) {
+            tileObject[param] = tile.params[param];
+        }
+        this[tile.category][posKey].push(tileObject);
+    }
+}
+
+module.exports = {
+    Tile, Sprite
+}
